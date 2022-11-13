b03b7bfd6698c70d90ae9431a70a464e5cf5b2dd
facundoMunoz084 <munozfacundo706@gmail.com>
2022-11-13 17:24:49 -0300

    se agrega el proyecto

---
+++ Listado de tareas.js
@@ -0,0 +1,17 @@
+[{
+    "id": 1,
+    "task": "ir de compras",
+    "completed" : false
+},{
+    "id": 2,
+    "tank": "lavar carro",
+    "completed": false 
+},{
+    "id": 3,
+    "tank": "cocinar",
+    "completed": true 
+},{
+    "id": 4,
+    "tank": "lavar ropa",
+    "completed": false 
+}] 
